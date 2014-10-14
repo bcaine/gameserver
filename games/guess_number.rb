1f8333f924caec121fb87628f8b0f154f16e6b30
Ben Caine <bcaine0@gmail.com>
2014-10-13 23:26:56 -0400

    Cleaned up guess_number

--- games/guess_number.rb
+++ games/guess_number.rb
@@ -20,12 +20,11 @@ class GuessNumber
 		end
 
 		guess = json_data["guess"].to_i
-		response = {}
 
 		if self.tries >= self.chances
 			won = false
 			done = true
-			response = { :won => self.won, :response => "Out of tries!", :tries => self.tries, :chances => self.chances }
+			return { :won => self.won, :response => "Out of tries!", :tries => self.tries, :chances => self.chances }
 		end
 
 		# Increment our try count
@@ -34,13 +33,11 @@ class GuessNumber
 		if self.number == guess
 			self.won = true
 			self.done = true
-			response = { :won => self.won, :done => self.done, :response => "Congrats, You won!", :tries => self.tries, :chances => self.chances }
+			{ :won => self.won, :done => self.done, :response => "Congrats, You won!", :tries => self.tries, :chances => self.chances }
 		elsif self.number > guess
-			response = { :response => "Guess a higher number.", :tries => self.tries, :chances => self.chances }
+			{ :response => "Guess a higher number.", :tries => self.tries, :chances => self.chances }
 		else
-			response = { :response => "Guess a lower number.", :tries => self.tries, :chances => self.chances }
+			{ :response => "Guess a lower number.", :tries => self.tries, :chances => self.chances }
 		end
-
-		response
 	end
 end
